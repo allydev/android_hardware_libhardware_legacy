4f50658b79c4167caaffcf8fcbbaccae87ff6208
Priya Komarlingam <knspriya@quicinc.com>
2009-11-18 19:02:19 -0800

    hardware/libhardware_legacy: Android SUPL NI client (Revision 2)
       Network Initiated support for AGPS on UMTS (SUPL) is added to Android framework and the
       GPS Hardware Abstraction Layer (HAL). It displays a notification message on the UI when
       receiving a notification only NI request, or a dialog box when receiving a
       notification/verification request.
       User responses are sent back to the HAL and GPS libraries (e.g. libloc_api).
    
        CRs-Fixed: 186692
    
    Conflicts:
    
    	include/hardware_legacy/gps_ni.h

--- include/hardware_legacy/gps.h
+++ include/hardware_legacy/gps.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (c) 2009, Code Aurora Forum. All rights reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 #ifndef _HARDWARE_GPS_H
